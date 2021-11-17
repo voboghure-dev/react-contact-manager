509cab3a4cabb9c9f2642b88bfb4213aca362cfe
Tapan Kumer Das <voboghure.dev@gmail.com>
2021-11-17 13:28:54 +0600

    Create skeleton components
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,16 @@
+import React from 'react';
 import './App.css';
+import Header from './Header'
+import AddContact from './AddContact';
+import ContactList from './ContactList';
 
 function App() {
   return (
-    <h1>Hello World from React!</h1>
+    <div className="ui container">
+      <Header />
+      <AddContact />
+      <ContactList />
+    </div>
   );
 }
 
